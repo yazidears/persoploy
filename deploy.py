3fd2c02beb67334a0893a7b8fcd9f9afaefedc0c
yzde <74788670+yazidears@users.noreply.github.com>
2023-05-29 09:37:30 +0200

    Added deploy.py content

--- deploy.py
+++ deploy.py
@@ -1,1 +1,18 @@
+# deploys
+import requests
+print("Deployer by yazidears")
+r = input("(f)ast mode or (d)etailed mode?")
+if r == "f":
+    print("Deploying now. Getting data from GitHub")
+    url = "https://raw.githubusercontent.com/yazidears/persoployer/"
+    print("Getting data-file.uwu")
+    file = url + "data-file.uwu"
+    resonse = requests.get(file)
+    if response.status_code == 200:
+        print("Download finished")
+        text_file = response.text
+    else:
+        print("An error ocurred. Launching undeployer now.")
+        os.system("python3 undeploy_urg.py")
+        
 
